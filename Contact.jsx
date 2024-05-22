3dbb43a673adc804518fd885d967cf1e3cebbb58
Raushan Kumar <113237821+raushankumar22@users.noreply.github.com>
2024-05-22 20:03:32 +0530

    📧adde place holder for mail

---
+++ Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contact() {
+  return <div id = 'contact'>
+    <h3> Send Me mail</h3>
+    <div className = "contact-input">
+        <input type="email" placeholder='abhi953434@gmail.com'/>
+        <a href='#'>Contact</a>
+    </div>
+  </div>;
+}
+
+export default Contact;
