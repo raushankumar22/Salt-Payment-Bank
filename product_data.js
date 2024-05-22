038092050ab330dd7728131e42dccc42656201e5
Raushan Kumar <113237821+raushankumar22@users.noreply.github.com>
2024-05-22 20:11:27 +0530

    Commit to mastwer

---
+++ product_data.js
@@ -0,0 +1,76 @@
+const product_card = [
+    {
+        id : 1,
+        product_name: "Etherum",
+        description:"Etherum is the Best",
+        price:350,
+        currency:"$",
+        thumb : "./images/ethereum.png"
+    },
+    {
+        id : 2,
+        product_name: "BYte",
+        description:"Byte shares running Down",
+        price:350,
+        currency:"$",
+        thumb : "./images/byte.png"
+    },
+    {
+        id : 3,
+        product_name: "Polygon",
+        description:"Polygon Streak is High",
+        price:350,
+        currency:"$",
+        thumb : "./images/polygon.png"
+    },
+    {
+        id : 4,
+        product_name: "Binance",
+        description:"Binance is Loosing there assests",
+        price:350,
+        currency:"$",
+        thumb : "./images/binance.png"
+    },
+    {
+        id : 5,
+        product_name: "Swipe",
+        description:"Swipt gave there all values",
+        price:350,
+        currency:"$",
+        thumb : "./images/swipe.png"
+    },
+    {
+        id : 6,
+        product_name: "Aurora",
+        description:"Aurora is slolly rising",
+        price:350,
+        currency:"$",
+        thumb : "./images/aurora.png"
+    },
+    {
+        id : 7,
+        product_name: "ETH",
+        description:"ETH make you share increse",
+        price:350,
+        currency:"$",
+        thumb : "./images/eth.png"
+    },
+    {
+        id : 8,
+        product_name: "Tezos",
+        description:"Tezos assests at a pick",
+        price:350,
+        currency:"$",
+        thumb : "./images/tezos.png"
+    },
+    {
+        id : 9,
+        product_name: "Neo",
+        description:"Neo give the all the Required value",
+        price:350,
+        currency:"$",
+        thumb : "./images/neo.png"
+    },
+
+]
+export default product_card;
